15fe90f91e239aaa71ccfc7b03bb110cfa545162
atib <atibuddin92@gmail.com>
2025-02-18 21:31:05 +0530

    Create NQueens.py

---
+++ NQueens.py
@@ -0,0 +1,42 @@
+""" The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+"""
+
+class Solution:
+    def _is_available(self, row, col, grid):
+        for r in range(self.N):
+            for c in range(self.N):
+                if grid[row][c] == 'Q' or grid[r][col] == 'Q':
+                    return False
+                if grid[r][c] == 'Q' and abs(r-row) == abs(c-col):
+                    return False
+        return True
+    def queen(self, row, n, grid):
+        if n == 0:
+            self.results.append(self.process_output(grid))
+            return
+        if row + n > self.N or row >= self.N:
+            return None
+        for col in range(self.N):
+            if self._is_available(row, col, grid) is True: # Available cell
+                grid[row][col] = 'Q'
+                self.queen(row+1, n-1, grid)
+                grid[row][col] = '.'
+    def process_output(self, grid) -> int:
+        """ 
+        input: [[Q X X],[X X Q],[X Q X]]
+        output: ['Q..', '..Q', '.Q.']        
+        """
+        ret = []
+        for row in grid:
+            s = ''.join(row)
+            ret.append(s)
+        return ret
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        grid = [['.'] * n for _ in range(n)]
+        self.results = []
+        self.N = len(grid)
+        self.queen(0,n,grid)
+        return self.results
