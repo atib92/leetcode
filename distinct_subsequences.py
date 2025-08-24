c7dcec5c9e5a6fe3fdad4d01f3d22054f477fc9f
atib <atibuddin92@gmail.com>
2025-08-24 09:07:11 +0530

    Update distinct_subsequences.py
    Added a top down DP approach and explanation. Moving to func within a func for easier variable sharing.

--- distinct_subsequences.py
+++ distinct_subsequences.py
@@ -39,35 +39,60 @@ Constraints:
 s and t consist of English letters.
 """
 class Solution:
-    def _numDistinct(self, i, j):
-        if i >= self.S:
-            return 1
-        elif j >= self.T:
-            return 0
+    def numDistinctRecursive(self, s: str, t: str) -> int:
+        """
+        subsequences of s which equals t
+        rabbbit rabbit
+        i       j
+        Keep two points i,j
+        if s[i] == s[j] 
+            Recurse twice :
+            1. Include s[i] so i+=1 and j+=1
+            2. Skip s[i] so just i+= 1 (we want some other s[i] to match t[j])
         else:
-            if self.memo[i][j] is None:
-                ans = 0
-                if self.s[i] == self.t[j]:
-                    ans += self._numDistinct(i+1, j+1)
-                ans += self._numDistinct(i, j+1)
-                self.memo[i][j] = ans
-            return self.memo[i][j]
-    def numDistinct(self, t: str, s: str) -> int:
+            same as #2 (no other option)
+        Everytime we reach j == len(t), we have completed j so 
+        Complexity: 2^N (two options for each letter in s) 2^1000 is too bad !!
+        But you can memoize the same <i,j> combination and make it O(N*M)
         """
-        We use a bottom up recursive solution with caching.
-        The idea is to find 't' in a subsequence of 's' so at
-        any step we have two options:
-        1. If s[i] == t[j], we can include s[i] and look for the remainder of t[j+1:] in s[i+1:]
-        2. Skip s[i] and continue to look for t[j:] in s[i+1:]
-        There will be repetetive calls for any (i, j) pair where caching is used.
-
-        This can also be approached via a more convntional bottoms up DP approach where:
-        dp[i][j] is the no. of ways to make t[:i] using s[:i] and we can extend this as follows:
-        dp[i][j] = dp[i-1][j-1] * (s[i] == t[j] ? 1 : 0) + dp[i-1][j] 
+        @cache
+        def _numDistinct(i, j):
+            if j == M:
+                return 1
+            elif i < N and j < M:
+                ret = 0
+                if s[i] == t[j]:
+                    # include
+                    ret  += _numDistinct(i+1, j+1)
+                # skip
+                ret += _numDistinct(i+1, j)
+                return ret
+            else:
+                return 0
+        N, M = len(s), len(t)
+        return _numDistinct(0,0)
+    def numDistinct(self, s: str, t: str) -> int:
         """
-        self.s = s
-        self.t = t
-        self.S = len(s)
-        self.T = len(t)
-        self.memo = [[None] * 1000 for _ in range(1000)]
-        return self._numDistinct(0,0)
+        A bottoms up DP approach. We define dp[i][j] as the number of distinct
+        subsequences of of s[:i] that matches t[:j]. This organically forces you
+        to create a N+1 X M+1 array and bootstrap it by setting dp[0][0] = 1
+        """
+        N, M = len(s), len(t)
+        dp = [[0]*(M+1) for _ in range(N+1)]
+        dp[0][0] = 1
+        for i in range(1, N+1):
+            for j in range(M+1):
+                if j > i:
+                    # Optimization: A shorter s' can never match t'
+                    continue
+                dp[i][j] += dp[i-1][j]
+                if j > 0 and s[i-1] == t[j-1]:
+                    dp[i][j] += dp[i-1][j-1]
+        return dp[N][M]
+"""
+Complexity
+Time complexity:
+O(N*M)
+Space complexity:
+O(N*M)
+"""
